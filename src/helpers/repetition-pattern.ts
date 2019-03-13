6fda5726e95ecc86f456b15a2083764ded5bd8f9
Drew Keller <drew@wimpyprogrammer.com>
2019-03-13 02:49:52 -0500

    Normalize simple quantifiers to range quantifiers
    Transform ?, +, and * quantifiers to their range quantifier equivalents to
    simplify downstream expansion logic.

--- src/helpers/repetition-pattern.ts
+++ src/helpers/repetition-pattern.ts
@@ -8,32 +8,17 @@ function assertNever(x: never): never {
 }
 
 function getNumOccurrences(quantifier: Quantifier): [number, number] {
-	let minOccurrences = 0;
-	let maxOccurrences = 100;
-
-	if (Guards.isSimpleQuantifier(quantifier)) {
-		if (quantifier.kind === '?') {
-			maxOccurrences = 1;
-		} else if (quantifier.kind === '+') {
-			minOccurrences = 1;
-		} else if (quantifier.kind === '*') {
-			// Keep defaults
-		} else {
-			/* istanbul ignore next */
-			throw new Error(`Unknown kind ${quantifier.kind}`);
-		}
-	} else if (Guards.isRangeQuantifier(quantifier)) {
-		minOccurrences = quantifier.from;
-
-		if ('to' in quantifier) {
-			maxOccurrences = quantifier.to;
-		}
+	/* istanbul ignore else */
+	if (Guards.isRangeQuantifier(quantifier)) {
+		const { from, to } = quantifier;
+		return [from, to !== undefined ? to : 100];
+	} else if (Guards.isSimpleQuantifier(quantifier)) {
+		const transformer = 'simpleQuantifierToRangeQuantifierTransform';
+		throw new Error(`"${quantifier.kind}" not removed by ${transformer}`);
 	} else {
 		/* istanbul ignore next */
 		assertNever(quantifier);
 	}
-
-	return [minOccurrences, maxOccurrences];
 }
 
 export function* expandRepetition(node: Repetition): IterableIterator<string> {
--- src/transforms/index.ts
+++ src/transforms/index.ts
@@ -1,6 +1,10 @@
 import { Handler } from 'regexp-tree';
 import metaToCharClassTransform from './meta-to-char-class-transform';
+import simpleQuantifierToRangeQuantifierTransform from './simple-quantifier-to-range-quantifier-transform';
 
-const transforms: Handler[] = [metaToCharClassTransform];
+const transforms: Handler[] = [
+	metaToCharClassTransform,
+	simpleQuantifierToRangeQuantifierTransform,
+];
 
 export default transforms;
