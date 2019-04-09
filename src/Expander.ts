ada57dcfd0b75c6fbad00f540f66fb01b48619a5
Drew Keller <drew@wimpyprogrammer.com>
2019-04-09 00:12:12 -0500

    Make expandNode() private
    Move expandNode() into Expander.  Do not export it publicly.

--- src/Expander.ts
+++ src/Expander.ts
@@ -1,14 +1,19 @@
+import { Expression } from 'regexp-tree/ast';
 import { expandAlternative } from './helpers/alternative-pattern';
 import { expandChar } from './helpers/char-pattern';
 import { expandCharacterClass } from './helpers/character-class-pattern';
 import { expandDisjunction } from './helpers/disjunction-pattern';
 import { expandBackreference, expandGroup } from './helpers/group-pattern';
 import { expandRepetition } from './helpers/repetition-pattern';
-import { expandNode } from './pattern';
 import sortRandom from './sorts/fisher-yates-random';
+import * as Guards from './types/regexp-tree-guards';
+
+/* istanbul ignore next */
+function assertNever(x: never): never {
+	throw new Error('Unexpected node type: ' + x);
+}
 
 class Expander {
-	public expandNode = expandNode;
 	protected expandAlternative = expandAlternative;
 	protected expandBackreference = expandBackreference;
 	protected expandChar = expandChar;
@@ -21,6 +26,34 @@ class Expander {
 		protected readonly flags: string,
 		protected readonly sort: <T>(options: T[]) => T[] = sortRandom
 	) {}
+
+	public *expandNode(
+		this: Expander,
+		node: Expression | null
+	): IterableIterator<string> {
+		if (node === null) {
+			yield '';
+		} else if (Guards.isAlternative(node)) {
+			yield* this.expandAlternative(node);
+		} else if (Guards.isAssertion(node)) {
+			yield '';
+		} else if (Guards.isBackreference(node)) {
+			yield* this.expandBackreference(node);
+		} else if (Guards.isChar(node)) {
+			yield* this.expandChar(node);
+		} else if (Guards.isCharacterClass(node)) {
+			yield* this.expandCharacterClass(node);
+		} else if (Guards.isDisjunction(node)) {
+			yield* this.expandDisjunction(node);
+		} else if (Guards.isGroup(node)) {
+			yield* this.expandGroup(node);
+		} else if (Guards.isRepetition(node)) {
+			yield* this.expandRepetition(node);
+		} else {
+			/* istanbul ignore next */
+			assertNever(node);
+		}
+	}
 }
 
 export default Expander;
--- src/pattern.ts
+++ src/pattern.ts
@@ -1,13 +1,6 @@
 import { parse, transform } from 'regexp-tree';
-import { Expression } from 'regexp-tree/ast';
 import Expander from './Expander';
 import transforms from './transforms/index';
-import * as Guards from './types/regexp-tree-guards';
-
-/* istanbul ignore next */
-function assertNever(x: never): never {
-	throw new Error('Unexpected node type: ' + x);
-}
 
 export function* expand(pattern: string | RegExp, sort?: Expander['sort']) {
 	if (!pattern) {
@@ -26,32 +19,4 @@ export function* expand(pattern: string | RegExp, sort?: Expander['sort']) {
 
 	const expander = new Expander(parsed.flags, sort);
 	yield* expander.expandNode(parsed.body);
-}
-
-export function* expandNode(
-	this: Expander,
-	node: Expression | null
-): IterableIterator<string> {
-	if (node === null) {
-		yield '';
-	} else if (Guards.isAlternative(node)) {
-		yield* this.expandAlternative(node);
-	} else if (Guards.isAssertion(node)) {
-		yield '';
-	} else if (Guards.isBackreference(node)) {
-		yield* this.expandBackreference(node);
-	} else if (Guards.isChar(node)) {
-		yield* this.expandChar(node);
-	} else if (Guards.isCharacterClass(node)) {
-		yield* this.expandCharacterClass(node);
-	} else if (Guards.isDisjunction(node)) {
-		yield* this.expandDisjunction(node);
-	} else if (Guards.isGroup(node)) {
-		yield* this.expandGroup(node);
-	} else if (Guards.isRepetition(node)) {
-		yield* this.expandRepetition(node);
-	} else {
-		/* istanbul ignore next */
-		assertNever(node);
-	}
 }
