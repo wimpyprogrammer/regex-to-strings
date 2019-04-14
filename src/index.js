ec416a5872817e287bf7f19846bae2fd729d21fa
Drew Keller <drew@wimpyprogrammer.com>
2019-04-14 18:20:12 -0500

    Add package entry point
    Use module.exports syntax for consistent behavior with import or require().
    
    Adjust tests to cover entry point.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { count, expand, expandAll, expandN, toRegExp } = require('./pattern');
+
+module.exports = {
+	count,
+	expand,
+	expandAll,
+	expandN,
+	toRegExp,
+};
