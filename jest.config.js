b60769836882fcd2d6d8810d4c721bd4f687e251
Drew Keller <drew@wimpyprogrammer.com>
2019-04-15 21:34:26 -0500

    Begin a tsconfig for tests
    Support a separate TypeScript configuration for tests.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
 	collectCoverageFrom: ['**/src/**/?*.(js|ts)', '!**/src/**/?*.d.ts'],
+	globals: {
+		'ts-jest': {
+			tsConfig: 'tsconfig.test.json',
+		},
+	},
 	moduleFileExtensions: ['js', 'json', 'ts'],
 	testMatch: ['**/src/**/?*.spec.(js|ts)'],
 	transform: {
